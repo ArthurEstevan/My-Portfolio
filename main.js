d6d6e4d6c441427afc7281d38b5385d95279e591
ArthurEstevan <arthur.estevan01@gmail.com>
2022-08-02 18:22:23 -0300

    Update | 02-08-22

--- main.js
+++ main.js
@@ -72,3 +72,102 @@ skillItems.forEach(skill => {
 window.addEventListener('scroll', () => {
     document.querySelector('nav').classList.toggle('show-box-shadow', window.scrollY > 0)
 })
+
+// dark mode
+
+function darkMode() {
+
+    // BODY
+    let body = document.body
+    body.classList.toggle("black-mode");
+    body.classList.toggle("h1")
+    body.classList.toggle("h2")
+    body.classList.toggle("h3")
+    body.classList.toggle("h4")
+    body.classList.toggle("h5")
+    body.classList.toggle("h6")
+
+    // NAV
+    let nav = document.querySelector("nav");
+    nav.classList.toggle("black-mode");
+
+    let lista = document.querySelector("nav");
+    lista.classList.toggle("ul");
+    lista.classList.toggle("efeito")
+
+    //LANDING
+    let circle = document.querySelector("section.landing .profile-area .outer-circle span:nth-child(1)");
+    circle.classList.toggle("circle")
+
+    let circle1 = document.querySelector("section.landing .profile-area .outer-circle span:nth-child(2)");
+    circle1.classList.toggle("circle")
+
+    let circle2 = document.querySelector("section.landing .profile-area .outer-circle span:nth-child(3)");
+    circle2.classList.toggle("circle")
+
+    let circle4 = document.querySelector("section.landing .profile-area .outer-circle span:nth-child(4)");
+    circle4.classList.toggle("circle")
+
+    //SOCIAL MEDIA
+
+    let media = document.querySelector("section.landing .socials a:nth-child(1)");
+    media.classList.toggle("media-social")
+
+    let media2 = document.querySelector("section.landing .socials a:nth-child(2)");
+    media2.classList.toggle("media-social")
+
+    let media3 = document.querySelector("section.landing .socials a:nth-child(3)");
+    media3.classList.toggle("media-social")
+
+    // ABOUT
+    let about = document.querySelector("section.about");
+    about.classList.toggle("black-mode");
+
+    let skill = document.querySelector("section.skills .skill .item");
+    skill.classList.toggle("black-mode");
+
+    let service = document.querySelector("section.services");
+    service.classList.toggle("black-mode");
+
+    // CARDS
+    let card = document.querySelector("section.services .container .service")
+    card.classList.toggle("black-mode")
+
+    let card1 = document.querySelector("section.services .container .service:nth-child(2)")
+    card1.classList.toggle("black-mode")
+
+    let card2 = document.querySelector("section.services .container .service:nth-child(3)")
+    card2.classList.toggle("black-mode")
+
+    // TESTEMONIAL
+    let testemonial = document.querySelector("section.testimonials");
+    testemonial.classList.toggle("black-mode");
+
+    let box = document.querySelector("section.testimonials .testimonial");
+    box.classList.toggle("black-mode");
+
+    let box1 = document.querySelector("section.testimonials .testimonial:nth-child(2)");
+    box1.classList.toggle("black-mode");
+
+    let box2 = document.querySelector("section.testimonials .testimonial:nth-child(3)");
+    box2.classList.toggle("black-mode");
+
+    let box3 = document.querySelector("section.testimonials .testimonial:nth-child(4)");
+    box3.classList.toggle("black-mode");
+
+    //FOOTER
+    let footer = document.querySelector("footer");
+    footer.classList.toggle("footer")
+    footer.classList.toggle("efeito")
+
+    //FOOTER SOCIAL
+    let footersocial1 = document.querySelector("footer .container .socials a:nth-child(1)");
+    footersocial1.classList.toggle("media-social")
+
+    let footersocial2 = document.querySelector("footer .container .socials a:nth-child(2)");
+    footersocial2.classList.toggle("media-social")
+
+    let footersocial3 = document.querySelector("footer .container .socials a:nth-child(3)");
+    footersocial3.classList.toggle("media-social")
+
+}
